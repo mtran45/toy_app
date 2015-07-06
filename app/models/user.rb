90bc328b472811d6ac454a76f2c2ca8abe76ef50
Michael Tran <mtran45@gmail.com>
2015-07-06 13:17:11 +0000

    Complete exercises: validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@
 #   .all method retrieves a list of all the users from the db
 class User < ActiveRecord::Base
   has_many :microposts
+  validates :name, presence: true
+  validates :email, presence: true
 end
